b0f0bbdf97379e348ab956879073a0625205ec72
Pedro Krapinski Neto <pedrokn@gmail.com>
2022-02-25 09:10:13 -0300

    commit

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -14,9 +14,9 @@
 <div class="navbar bg-base-100 z-50">
     <div class="navbar-start">
       <div class="dropdown">
-        <label class="btn btn-ghost lg:hidden" on:click={handleClick}>
+        <button class="btn btn-ghost lg:hidden" on:click={handleClick}>
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-        </label>
+        </button>
         {#if open}
         <ul tabindex="0" class="absolute bg-gray-50 w-56 text-center p-6 rounded-xl z-50" transition:slide>
           <li class="mt-2" on:click={handleClick}><a href="#about">Despre noi</a></li>
